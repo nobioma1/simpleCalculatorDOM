9026d1c91d989045f1f443a64ee80f45c5403e14
Noble Obioma <nobioma1@gmail.com>
2019-05-12 00:46:27 +0100

    delete the last character in the display

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ class Calculator {
     this.clickEvent = buttons.addEventListener('click', (event) => this.action(event));
     this.primaryDisplay = document.querySelector('.primary-display');
     this.secondaryDisplay = document.querySelector('.secondary-display');
+    this.temp = [];
   }
 
   action(event) {
@@ -11,15 +12,27 @@ class Calculator {
       case text === 'clear':
         this.secondaryDisplay.textContent = ''
         this.primaryDisplay.textContent = '0';
+        this.temp = []
         break;
-      default: 
+      case text === 'del':
+        this.delete();
+        break;
+      default:
         this.showPrimaryDisplay(text);
     }
-    console.log(event.target.id)
+  }
+
+  delete(){
+    let mainText = this.primaryDisplay.textContent;
+    this.primaryDisplay.textContent = mainText.slice(0, mainText.length-1)
   }
 
   showPrimaryDisplay(text) {
-    this.primaryDisplay.textContent += text;
+    if (this.primaryDisplay.textContent === '0') {
+      this.primaryDisplay.textContent = text;
+    } else {
+      this.primaryDisplay.textContent += text;
+    }
   }
 }
 
