94d660236b2b34d8d1b9395f27ac450c4befcc43
Noble Obioma <nobioma1@gmail.com>
2019-05-12 00:35:27 +0100

    - add display to screen (primary) - clear text from display

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 class Calculator {
   constructor(buttons) {
     this.clickEvent = buttons.addEventListener('click', (event) => this.action(event));
+    this.primaryDisplay = document.querySelector('.primary-display');
+    this.secondaryDisplay = document.querySelector('.secondary-display');
   }
 
   action(event) {
+    let text = event.target.id;
+    switch (true) {
+      case text === 'clear':
+        this.secondaryDisplay.textContent = ''
+        this.primaryDisplay.textContent = '0';
+        break;
+      default: 
+        this.showPrimaryDisplay(text);
+    }
     console.log(event.target.id)
+  }
+
+  showPrimaryDisplay(text) {
+    this.primaryDisplay.textContent += text;
   }
 }
 
