455f1d5ef7f44edfc84142edf6c8dea44ccca484
Noble Obioma <nobioma1@gmail.com>
2019-05-12 08:20:51 +0100

    ft: ADD: - format file structure - remove temp properties and add currentEntryValue, currentResult properties - add array of operations in action method - add conditional case to call operate method passing argument of current operation text - add operate method to push converted entry to number to array currentEntryValue - add switch statement wit case + - case + to reduce the array currentEntryValue to sum and call update display DOM - add method to ClearDisplay when CE is clicked

--- main.js
+++ main.js
@@ -1,38 +1,78 @@
 class Calculator {
   constructor(buttons) {
-    this.clickEvent = buttons.addEventListener('click', (event) => this.action(event));
+    this.clickEvent = buttons.addEventListener('click', event =>
+      this.action(event)
+    );
     this.primaryDisplay = document.querySelector('.primary-display');
     this.secondaryDisplay = document.querySelector('.secondary-display');
-    this.temp = [];
+    this.currentEntryValue = [];
+    this.currentResult = 0;
   }
 
   action(event) {
     let text = event.target.id;
+    let operations = ['+', '/', 'x', '-'];
     switch (true) {
       case text === 'clear':
-        this.secondaryDisplay.textContent = ''
-        this.primaryDisplay.textContent = '0';
-        this.temp = []
+        this.clearDisplay();
         break;
       case text === 'del':
         this.delete();
+        break;
+      case operations.includes(text):
+        this.operate(text);
         break;
       default:
         this.showPrimaryDisplay(text);
     }
   }
 
-  delete(){
-    let mainText = this.primaryDisplay.textContent;
-    this.primaryDisplay.textContent = mainText.slice(0, mainText.length-1)
+  operate(operation) {
+    this.currentEntryValue.push(Number(this.primaryDisplay.textContent));
+
+    switch (operation) {
+      case '+':
+        this.currentResult = this.currentEntryValue.reduce(
+          (accumulator, currV) => {
+            return accumulator + currV;
+          },
+          0
+        );
+      this.clearPrimaryCalculate();
+      
+    }
+    //console
+    console.log({ result: this.currentResult, value: this.currentEntryValue });
   }
 
-  showPrimaryDisplay(text) {
+  clearDisplay() {
+    this.secondaryDisplay.textContent = '';
+    this.primaryDisplay.textContent = '0';
+    this.currentEntryValue = [];
+    this.currentResult = 0;
+  }
+
+  delete() {
+    let mainText = this.primaryDisplay.textContent;
+    this.primaryDisplay.textContent =
+      mainText.length > 1 ? mainText.slice(0, mainText.length - 1) : 0;
+  }
+
+  showPrimaryDisplay(currentEntry) {
     if (this.primaryDisplay.textContent === '0') {
-      this.primaryDisplay.textContent = text;
+      this.primaryDisplay.textContent = currentEntry;
     } else {
-      this.primaryDisplay.textContent += text;
+      this.primaryDisplay.textContent += this.currentResult;
     }
+  }
+
+  clearPrimaryCalculate() {
+    if (this.currentResult !== 0) {
+      this.secondaryDisplay.textContent = `${this.currentResult}`;
+    } else {
+      this.secondaryDisplay.textContent = this.currentEntryValue;
+    }
+    this.primaryDisplay.textContent = '0';
   }
 }
 
